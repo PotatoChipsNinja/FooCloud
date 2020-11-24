8d4f521e7c787ba1e727283a10e8223fd33c97fa
PotatoChipsNinja <shiyuhui2000@126.com>
2020-11-24 11:54:16 +0800

    frontend: framework and sidebar

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+M.AutoInit();
+
+function func() {
+  var side = $('#side');
+  var content = $('#content');
+  if (side.is(':visible')) {
+    content.animate({left: 0}, 200);
+    side.animate({width: 0, opacity: 0}, 200, () => {
+      side.hide();
+    });
+  } else {
+    side.show();
+    content.animate({left: '15rem'}, 200);
+    side.animate({width: '15rem', opacity: 1}, 200);
+  }
+}
