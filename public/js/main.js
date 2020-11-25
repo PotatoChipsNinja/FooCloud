7e7f4c12e04161a2f7f9d9bd5b5f17f2b96ee55a
PotatoChipsNinja <shiyuhui2000@126.com>
2020-11-25 22:12:08 +0800

    frontend: night mode

--- public/js/main.js
+++ public/js/main.js
@@ -9,3 +9,36 @@ function sidebar() {
     side.show();
   }
 }
+
+function nightMode() {
+  $('body').append(css);
+  styleObj = $('body').children().last();
+  $('#switch-mode').attr('onclick', 'dayMode();');
+  $('#switch-mode').children().text('brightness_5');
+}
+
+function dayMode() {
+  styleObj.remove();
+  $('#switch-mode').attr('onclick', 'nightMode();');
+  $('#switch-mode').children().text('brightness_4');
+}
+
+var ts = [
+  'img',
+  'video',
+  'iframe',
+  'embed',
+  'object',
+  '[style*="background:url"]',
+  '[style*="background-image:url"]',
+  '[style*="background: url"]',
+  '[style*="background-image: url"]',
+  '[style*="background-image"][style*="image-set"]'
+];
+ts = '#content,#side,' + ts.join(',') + '{filter:invert(100%);}'
+    + ts.map(function (p) { return ts.map(function (p2) { return p + ' ' + p2 }).join(',') }).join(',') + '{filter:invert(0%);}'
+    + 'body{background-color:rgba(0,0,0,0.9)!important;}:root,html{background-color:rgba(0,0,0,0)!important;}';
+ 
+var css = document.createElement('style');
+css.innerHTML = ts;
+var styleObj;
