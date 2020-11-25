d3cbc3d0cbc8873439a15c5fbbbb86777811ceb9
PotatoChipsNinja <shiyuhui2000@126.com>
2020-11-25 13:27:55 +0800

    frontend

--- public/js/disk.js
+++ public/js/disk.js
@@ -1,7 +1,3 @@
-if (!localStorage.getItem('token')) {
-  window.location.href = '/login';
-}
-
 $(document).ready(() => {
   M.Dropdown.init($('.dropdown-trigger'), {});
 });
@@ -19,7 +15,35 @@ function setPath(p) {
       $('#path-list').append(`<a class="waves-effect waves-teal btn-flat path-item" onclick="setPath('${currPath}');">${arr[i]}</a>`);
     }
   }
+  loadList();
 }
 
+function sort(s) {
+  sort = s;
+  loadList();
+}
+
+function loadList() {
+  $.get({
+    url: '/api/disk/directory',
+    headers: { Authorization: 'Bearer ' + token },
+    data: { path: path, sort: sort },
+    success: (res) => {
+      console.log('success', res);
+    },
+    error: (res) => {
+      let code = res.responseJSON.code;
+      if (code == 101) {
+        window.location.href = '/login';
+      }
+    }
+  });
+}
+
+var token = localStorage.getItem('token');
 var path = '/';
+var sort = 0;
+if (!token) {
+  window.location.href = '/login';
+}
 setPath(path);
