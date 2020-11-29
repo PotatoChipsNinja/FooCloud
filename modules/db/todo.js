b0e7ac7d5f1acd90c834d634b923b458390ef582
PotatoChipsNinja <shiyuhui2000@126.com>
2020-11-29 16:33:49 +0800

    add todo API

--- modules/db/todo.js
+++ modules/db/todo.js
@@ -1,8 +1,81 @@
 // modules/db/todo.js: 数据库待办事项操作
 
+const uuid = require('uuid')
 const db = require('./base')
+
+// 添加待办事项
+function add(username, content, callback) {
+  let todo = db.gDb.collection('todo')
+  let newTodo = { UUID: uuid.v1(), username: username, content: content, time: new Date().getTime(), finished: false }
+  share.insertOne(newTodo, (err, result) => {
+    callback(err)
+  })
+}
+
+// 获取待办事项列表
+function list(username, callback) {
+  let todo = db.gDb.collection('todo')
+  share.find({ username: username }).toArray((err, result) => {
+    callback(err, result)
+  })
+}
+
+// 切换待办事项完成状态
+function switchStatus(username, UUID, callback) {
+  let todo = db.gDb.collection('todo')
+  share.find({ username: username, UUID: UUID }).toArray((err, result) => {
+    if (err) {
+      callback({ error: 'Internal Error', code: 104 })
+      return
+    }
+
+    if (result.length == 0) {
+      // 不存在该待办事项
+      callback({ error: 'TODO Not Exist', code: 501 })
+      return
+    }
+
+    // 修改表项
+    todo.updateOne({ username: username, UUID: UUID }, { finished: !result[0].finished }, (err, result) => {
+      if (err) {
+        callback({ error: 'Internal Error', code: 104 })
+      } else {
+        callback()
+      }
+    })
+  })
+}
+
+// 删除待办事项
+function deleteItem(username, UUID, callback) {
+  let todo = db.gDb.collection('todo')
+  share.find({ username: username, UUID: UUID }).toArray((err, result) => {
+    if (err) {
+      callback({ error: 'Internal Error', code: 104 })
+      return
+    }
+
+    if (result.length == 0) {
+      // 不存在该待办事项
+      callback({ error: 'TODO Not Exist', code: 501 })
+      return
+    }
+
+    // 删除表项
+    todo.deleteOne({ username: username, UUID: UUID }, (err, result) => {
+      if (err) {
+        callback({ error: 'Internal Error', code: 104 })
+      } else {
+        callback()
+      }
+    })
+  })
+}
 
 // 对外部暴露模块
 module.exports = {
-  
+  add,
+  list,
+  switchStatus,
+  deleteItem
 }
--- routes/api/todo.js
+++ routes/api/todo.js
@@ -5,6 +5,80 @@ const db = require('../../modules/db/todo')
 
 const router = express.Router()
 
+// 添加待办事项
+router.post('/add', express.urlencoded({ extended: false }), (req, res) => {
+  let content = req.body.content
+
+  if (!content) {
+    // 缺少必要参数
+    res.status(400).send({ error: 'Parameter Error', code: 103 })
+    return
+  }
+
+  db.add(req.username, content, (err) => {
+    if (err) {
+      res.status(500).send({ error: 'Internal Error', code: 104 })
+    } else {
+      res.send({ success: true })
+    }
+  })
+})
+
+// 获取待办事项列表
+router.get('/list', (req, res) => {
+  db.list(req.username, (err, items) => {
+    if (err) {
+      res.status(500).send({ error: 'Internal Error', code: 104 })
+    } else {
+      items.forEach(obj => delete(obj.username))
+      // 按时间降序排列
+      items.sort((a, b) => {
+        return b.time - a.time
+      })
+      let count = items.length
+      res.send({ count: count, items: items })
+    }
+  })
+})
+
+// 切换待办事项完成状态
+router.post('/switch', express.urlencoded({ extended: false }), (req, res) => {
+  let UUID = req.body.UUID
+
+  if (!UUID) {
+    // 缺少必要参数
+    res.status(400).send({ error: 'Parameter Error', code: 103 })
+    return
+  }
+
+  db.switchStatus(req.username, UUID, (err) => {
+    if (err) {
+      res.status(err.code == 104 ? 500 : 403).send(err)
+    } else {
+      res.send({ success: true })
+    }
+  })
+})
+
+// 删除待办事项
+router.post('/delete', express.urlencoded({ extended: false }), (req, res) => {
+  let UUID = req.body.UUID
+
+  if (!UUID) {
+    // 缺少必要参数
+    res.status(400).send({ error: 'Parameter Error', code: 103 })
+    return
+  }
+
+  db.deleteItem(req.username, UUID, (err) => {
+    if (err) {
+      res.status(err.code == 104 ? 500 : 403).send(err)
+    } else {
+      res.send({ success: true })
+    }
+  })
+})
+
 router.use((req, res) => {
   res.status(404).send({ error: 'Wrong API URL', code: 102 })
 })
